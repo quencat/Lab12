2a16735f127950fd5b710a66b886f91edd31ca8c
Test <xingyub2@illinois.edu>
2018-04-17 12:34:52 -0500

    lab12

--- app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -3,14 +3,19 @@ package edu.illinois.cs.cs125.lab12;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -33,10 +38,21 @@ public final class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
         // Set up the queue for our API requests
         requestQueue = Volley.newRequestQueue(this);
 
         setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+                                      public void onClick(final View v) {
+                                          TextView x = findViewById(R.id.button);
+                                          x.setVisibility(View.VISIBLE);
+                                          x.setText("clicked");
+                                      }
+    }
+        );
 
         startAPICall();
     }
@@ -64,6 +80,13 @@ public final class MainActivity extends AppCompatActivity {
                         public void onResponse(final JSONObject response) {
                             try {
                                 Log.d(TAG, response.toString(2));
+                                JSONArray weather = response.getJSONArray("weather");
+                                JSONObject main = weather.getJSONObject(0);
+                                String wanted = main.getString("main");
+                                TextView textView = findViewById(R.id.display);
+                                textView.setText(wanted);
+                                Button any = findViewById(R.id.button);
+                                any.setText(wanted);
                             } catch (JSONException ignored) { }
                         }
                     }, new Response.ErrorListener() {
